7ae2cb4c81730c3d2370a33c43bf9172c0645bde
prashantbohra01 <prashantb4157@gmail.com>
2024-02-22 00:27:03 -0800

    DAY-1

---
+++ Day 1 ( Drum-Kit )/script.js
@@ -0,0 +1,72 @@
+function playSound(){
+    var sound = document.getElementById("01");
+    sound.play();
+    setTimeout(function(){
+        sound.pause();
+        sound.currentTime=0;
+    }, 2000)
+}
+function playSound2(){
+    var sound = document.getElementById("02");
+    sound.play();
+    setTimeout(function(){
+        sound.pause();
+        sound.currentTime=0;
+    }, 2500)
+}
+function playSound3(){
+    var sound = document.getElementById("03");
+    sound.play();
+    setTimeout(function(){
+        sound.pause();
+        sound.currentTime=0;
+    }, 2000)
+}
+function playSound4(){
+    var sound = document.getElementById("04");
+    sound.play();
+    setTimeout(function(){
+        sound.pause();
+        sound.currentTime=0;
+    }, 2000)
+}
+function playSound5(){
+    var sound = document.getElementById("05");
+    sound.play();
+    setTimeout(function(){
+        sound.pause();
+        sound.currentTime=0;
+    }, 1000)
+}
+function playSound6(){
+    var sound = document.getElementById("06");
+    sound.play();
+    setTimeout(function(){
+        sound.pause();
+        sound.currentTime=0;
+    }, 2000)
+}
+function playSound7(){
+    var sound = document.getElementById("07");
+    sound.play();
+    setTimeout(function(){
+        sound.pause();
+        sound.currentTime=0;
+    }, 3000)
+}
+function playSound8(){
+    var sound = document.getElementById("08");
+    sound.play();
+    setTimeout(function(){
+        sound.pause();
+        sound.currentTime=0;
+    }, 3000)
+}
+function playSound9(){
+    var sound = document.getElementById("09");
+    sound.play();
+    setTimeout(function(){
+        sound.pause();
+        sound.currentTime=0;
+    }, 3000)
+}
