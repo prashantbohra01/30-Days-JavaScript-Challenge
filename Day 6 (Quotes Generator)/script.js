ffff4e631b6e158d77f9d6dabb68c105a5c07b4a
prashantbohra01 <prashantb4157@gmail.com>
2024-02-28 22:49:18 +0530

    Day 6 (Quote Generator)

---
+++ Day 6 (Quotes Generator)/script.js
@@ -0,0 +1,14 @@
+const quote = document.querySelector('#quote');
+const author = document.querySelector('#author');
+const btn = document.querySelector('#loadQuote');
+
+btn.addEventListener('click', getQuote);
+
+function getQuote(){
+    fetch("http://quotable.io/random")
+    .then(res => res.json())
+    .then(data => {
+        quote.innerHTML = data.content;
+        author.innerHTML = data.author;
+    })
+}
